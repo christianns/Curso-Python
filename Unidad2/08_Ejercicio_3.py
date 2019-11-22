e0adfa0e14b36e9ae8817c13a59c6f43ce671dd2
cnavarrete <christianavarrete@gmail.com>
2019-11-22 09:44:17 -0300

    Actulazacion de ejercicio 3 - unidad2

---
+++ Unidad2/08_Ejercicio_3.py
@@ -0,0 +1,19 @@
+# Problema 03: Dado un carácter determine si es una Vocal o no.
+# Análisis: Para la solución de este problema, se requiere que
+# usuario ingrese un carácter y el sistema verifique si es un
+# vocal o no
+
+print ("Ingrese caracter:")
+vocal = input()
+if vocal == str("a"):
+    print ("El caracter ingresado", vocal + " una vocal")
+elif vocal == str("e"):
+    print ("El caracter ingresado", vocal + " una vocal")
+elif vocal == str("i"):
+    print ("El caracter ingresado", vocal + " una vocal")
+elif vocal == str("o"):
+    print ("El caracter ingresado", vocal + " una vocal")
+elif vocal == str("u"):
+    print ("El caracter ingresado", vocal + " una vocal")
+else:
+    print ("El caracter ingresado no es una vocal")
